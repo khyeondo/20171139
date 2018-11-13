2e3961873a313cc7c03ef65814f63d87087ae53d
김현도 <20171139@vision.hoseo.edu>
2018-11-13 13:00:10 +0900

    PP05.AnimatingSprite

--- GameFrameWork/PP01.HelloSDL/Game.cpp
+++ GameFrameWork/PP01.HelloSDL/Game.cpp
@@ -14,12 +14,20 @@ bool Game::init(const char * title, int xpos, int ypos, int width, int height, b
 	else
 		return false;
 
-	SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/rider.bmp");
+	SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/animate.bmp");
 	m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
 
 	SDL_FreeSurface(pTempSurface);
 
-	SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
+	m_sourceRectangle.w = 128;
+	m_sourceRectangle.h = 82;
+
+	m_destinationRectangle.x = m_sourceRectangle.x = 0;
+	m_destinationRectangle.y = m_sourceRectangle.y = 0;
+	m_destinationRectangle.w = m_sourceRectangle.w;
+	m_destinationRectangle.h = m_sourceRectangle.h;
+
+	//SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
 
 	m_bRunning = true;
 
@@ -28,10 +36,6 @@ bool Game::init(const char * title, int xpos, int ypos, int width, int height, b
 
 void Game::render()
 {
-	m_destinationRectangle.x = m_sourceRectangle.x = 0;
-	m_destinationRectangle.y = m_sourceRectangle.y = 0;
-	m_destinationRectangle.w = m_sourceRectangle.w;
-	m_destinationRectangle.h = m_sourceRectangle.h;
 
 	SDL_RenderClear(m_pRenderer);
 
@@ -43,6 +47,7 @@ void Game::render()
 
 void Game::update()
 {
+	m_sourceRectangle.x = 128 * int(((SDL_GetTicks() / 100) % 6));
 }
 
 void Game::handleEvents()
