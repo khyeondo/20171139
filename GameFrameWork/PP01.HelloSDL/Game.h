40ebfe9a33946c35a87d3afa37aac537bb81d892
k k <khd1088@naver.com>
2018-11-13 20:54:10 +0900

    PP11.AbstractClass

--- GameFrameWork/PP01.HelloSDL/Game.h
+++ GameFrameWork/PP01.HelloSDL/Game.h
@@ -2,12 +2,29 @@
 
 #include <SDL.h>
 #include <vector>
+#include <iostream>
 #include "GameObject.h"
 #include "Player.h"
 class Game
 {
-public:
+private:
 	Game() {}
+	// create the s_pInstance member variable
+	static Game* s_pInstance;
+
+public:
+	static Game* Instance()
+	{
+		if (s_pInstance == 0)
+		{
+			s_pInstance = new Game();
+			return s_pInstance;
+		}
+		return s_pInstance;
+	}
+
+public:
+	//Game() {}
 	~Game() {}
 	bool init(const char* title, int xpos, int ypos,
 		int width, int height, bool fullscreen);
@@ -18,6 +35,7 @@ public:
 	void clean();
 	bool running() { return m_bRunning; }
 	
+	SDL_Renderer* getRenderer() const { return m_pRenderer; }
 private:
 	SDL_Window* m_pWindow;
 	SDL_Renderer* m_pRenderer;
@@ -26,9 +44,5 @@ private:
 	int m_currentFrame;
 
 	std::vector<GameObject*> m_gameObjects;
-
-	GameObject* m_go;
-	GameObject* m_player;
-	GameObject* m_enemy;
-
 };
+typedef Game TheGame;
