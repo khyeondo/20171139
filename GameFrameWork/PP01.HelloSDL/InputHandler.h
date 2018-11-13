ab0fef669168e741be286b786001f33e44a1afe2
k k <khd1088@naver.com>
2018-11-13 21:17:13 +0900

    PP14.MInputHandler

--- GameFrameWork/PP01.HelloSDL/InputHandler.h
+++ GameFrameWork/PP01.HelloSDL/InputHandler.h
@@ -1,6 +1,16 @@
 #pragma once
 
 #include <SDL.h>
+#include <vector>
+
+class Vector2D;
+
+enum mouse_buttons
+{
+	LEFT = 0,
+	MIDDLE = 1,
+	RIGHT = 2
+};
 
 class InputHandler
 {
@@ -12,10 +22,22 @@ public:
 		}
 		return s_pInstance;
 	}
+
 	void update();
 	void clean();  // 디바이스 관련 초기화된 부분을 clear 
 
 	bool isKeyDown(SDL_Scancode key);
+	bool getMouseButtonState(int buttonNumber);
+	Vector2D* getMousePosition();
+
+private:
+	void onMouseMove(SDL_Event& event);
+	void onMouseButtonDown(SDL_Event& event);
+	void onMouseButtonUp(SDL_Event& event);
+
+	void onKeyUp();
+	void onKeyDown();
+
 private:
 	InputHandler();
 	~InputHandler() {}
@@ -23,6 +45,12 @@ private:
 
 	const Uint8* m_keystates;
 
+
+	// mouse buttion events								
+	std::vector<bool> m_mouseButtonStates;
+
+	// mouse motion event					
+	Vector2D* m_mousePosition;
 };
 
 
